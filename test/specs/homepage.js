07605321f0eeb0bb9e1c156df061703ec365d022
Ryan Volker <ryanvolker@gmail.com>
2024-07-07 15:09:27 -0600

    Add files via upload

---
+++ test/specs/homepage.js
@@ -0,0 +1,12 @@
+
+
+describe('test home page', async()=> 
+{
+    it ('Test Home Page', async()=> 
+    {
+        await browser.url("https://www.homedepot.com");
+        console.log(await browser.getTitle());
+        //testing out git hub changes with a commenthbubghubhub
+    }) 
+    
+})
